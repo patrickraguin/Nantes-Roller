017581f347d1ad6158f60599ada32fb8370ff7c8
admin <patrick.raguin@gmail.com>
2016-07-25 22:00:00 +0200

    [VP] Activated VersionPress 3.0.2
    VP-Action: versionpress/activate/3.0.2

---
+++ wp-config.common.php
@@ -0,0 +1,8 @@
+<?php
+// Configuration common to all VersionPress environments, included from `wp-config.php`.
+// Learn more at https://docs.versionpress.net/en/getting-started/configuration
+
+
+//Plugin VersionPress
+define('VP_VPDB_DIR', WP_CONTENT_DIR . '/vpdb');
+define('VP_PROJECT_ROOT', ABSPATH);
